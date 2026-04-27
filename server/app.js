8ca55b53450a86fcb5e470ce74de33d0f7e00099
sowmya-9005 <saisowmyasreedonthu@gmail.com>
2026-04-27 21:13:47 +0530

    fix: remove static file serving from Express

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const path = require("path");
 const predictRouter = require("./routes/predict");
 require("dotenv").config();
 
@@ -14,14 +13,6 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((err) => console.error("MongoDB error:", err));
 
 app.use("/api", predictRouter);
-
-// Serve React build in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-  });
-}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
